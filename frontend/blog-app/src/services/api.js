f74082737908384254afdec1b8dc5b986021b373
Alefe Bispo <alefebsp@hotmail.com>
2022-10-04 20:18:06 -0300

    added api local addres

--- frontend/blog-app/src/services/api.js
+++ frontend/blog-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://sapphire-app-backend.herokuapp.com',
+  baseURL: 'http://localhost:3001',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*'
